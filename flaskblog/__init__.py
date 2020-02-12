d19c1b8b45bd0f130353c4a9accb510048808e54
Smit Patel <PSmit29@gmail.com>
2020-02-12 19:55:42 +0530

    Added config.py file to our package flaskblog. We will now load configurations of the application from the object of class config. Anything that was app.config is now moved to config.py file. We have also moved the secret key and database uri to env variables so that they can remain hidden.

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,24 +1,19 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flaskblog.config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '17de3735f35c7ec699c164a6854299bc'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+
 mail = Mail(app)
 
 from flaskblog.users.routes import users
